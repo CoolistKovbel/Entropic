a85d39d1e2da44d863f144b9db0b14fa444eb9a8
Lyubomyr Kovbel <shpintz@Lyubomyrs-MacBook-Pro.local>
2024-05-23 09:15:20 +0300

    slow

--- app/(site)/mint/page.tsx
+++ app/(site)/mint/page.tsx
@@ -49,26 +49,24 @@ const page = async () => {
 
   // Grab latest collection
   
-  const grabLatestCollections = async () => {
-    try {
-      console.log("ouwwwwwwwwwe");
-      const slow = await grabLatestCollections()
-    } catch (error) {
-      console.log(error);
-    }
-  };
+
 
   const gg = await grabRecentCollection();
+
+  console.log(gg, "Heay")
 
   return (
     <section className="w-full min-h-screen p-4 ">
 
       <header className="w-full bg-[#222] mb-10 rounded-lg">
         <h2 className="text-2xl p-3 font-bold bg-[#333]">Latest mint</h2>
+
         {/* Collection */}
         {gg.length > 0 && (
           <div className="flex items-center gap-4 p-4 bg-[#444] flex-col justify-center">
+
             <div className="w-full">
+
               <h2 className="text-4xl mb-2 text-center">
                 {gg[0].collectionName}
               </h2>
@@ -76,6 +74,7 @@ const page = async () => {
               <div className="w-[300px] h-[300px] relative mx-auto">
                 <Image src={gg[0].image} alt="art concept nft" fill />
               </div>
+
             </div>
 
             <div className="w-full flex flex-col gap-4 p-4 bg-[#222] drop-shadow-lg rounded-lg">
@@ -98,17 +97,52 @@ const page = async () => {
                 Learn More
               </a>
             </div>
+
           </div>
         )}
+
       </header>
 
       <HandleNFTListing userIsLogged={userIsLogged} />
 
       <div className="w-full flex items-center flex-col w-full h-[800px] overflow-auto gap-4">
 
-        <CollectionListing collProps={grabLatestCollections} />
-
         <div
+            key={crypto.randomUUID()}
+            className="p-4 bg-[#222] w-full rounded-lg flex items-center justify-between"
+          >
+
+            <header className="w-[70%] flex items-center flex-col">
+
+              <div className="text-center mt-10">
+                <div className="flex items-center justify-between gap-3 rounded-lg w-80% mb-4">
+                  <p className="bg-[#111] p-3">
+                    views: <span>1000</span>
+                  </p>
+                  <p className="bg-[#111] p-3">
+                    interest: <span>10</span>
+                  </p>
+                  <p className="bg-[#111] p-3">
+                    holders: <span>10</span>
+                  </p>
+                </div>
+                <Link
+                  href="/"
+                  className="w-full bg-[#000] p-3 hover:bg-[#555] rounded-lg"
+                >
+                  Read More
+                </Link>
+              </div>
+
+            </header>
+
+            <div className="w-[300px] h-[300px] relative">
+              <Image src={gg[0].image} alt="slow life" fill />
+            </div>
+            
+          </div>
+
+          <div
             key={crypto.randomUUID()}
             className="p-4 bg-[#222] w-full rounded-lg flex items-center justify-between"
           >
--- app/components/collectionList/collectionlisting.tsx
+++ app/components/collectionList/collectionlisting.tsx
@@ -1,22 +1,34 @@
 "use server";
 
-interface CollectionListingProps {
-  collProps: any;
-}
+import { grabLatestCollections } from "@/app/lib/action";
 
-const CollectionListing = async ({ collProps }: CollectionListingProps) => {
-  const gg = await collProps;
+const CollectionListing = async () => {
+  const grabALLCollections = async () => {
+    try {
+      const wg = await grabLatestCollections();
+
+      return wg;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const starter = await grabALLCollections();
+ 
+  console.log("Why this is not ", starter);
 
   return (
-    <div className="bg-[#222] p-4">
-      {gg.map((item: any) => {
-        <div>
-          <div>
-            <h1 className="text-3xl font-bold mb-2">{item.collectionName}</h1>
-            <p className="text-md">{item.collectionDescription}</p>
+    <div className="bg-[#222] p-10">
+      {starter.map((item: any) => {
+        return (
+          <div key={crypto.randomUUID()}>
+            <div>
+              <h1 className="text-3xl font-bold mb-2">{item.collectionName}</h1>
+              <p className="text-md">{item.collectionDescription}</p>
+            </div>
+            {item.collectionContractAddress}
           </div>
-          {item.collectionContractAddress}
-        </div>;
+        );
       })}
     </div>
   );
